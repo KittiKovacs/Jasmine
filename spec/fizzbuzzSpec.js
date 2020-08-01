f1ad16ff43a91c27f5706cc8f239136bba769086
Kitti Kovacs <bambula86@gmail.com>
2020-08-01 12:41:13 +0000

    fizzbuzz initial commit

---
+++ spec/fizzbuzzSpec.js
@@ -0,0 +1,19 @@
+describe("My Fizzbuzz", function() {
+    beforeEach(function(){ 
+        number = new fizzbuzz;
+    });
+    
+
+    describe("test is number is divisible by 3 and 5, then return 'FizzBuzz'", function() {
+        it("should return 'FizzBuzz'", function() {
+            var expect=15;
+            expect(calc.value).toBe();
+        });
+        it("should return 'Fizz'", function() {
+            var expect=15;
+            expect(calc.value).toBe();
+        });
+        it("should return 'Buzz'", function() {
+            var expect=15;
+            expect(calc.value).toBe();
+        });
