c0fcd08f884eb09172a90612222577de6e69dea7
Kitti Kovacs <bambula86@gmail.com>
2020-08-02 10:39:40 +0000

    fizzbuzz complete

--- scripts/fizzbuzz.js
+++ scripts/fizzbuzz.js
@@ -1,17 +1,14 @@
-fizzbuzz= function(number){
+fizzBuzz= function(number){
 if (number % 3 === 0 && number % 5 === 0) {
     return "FizzBuzz";
 }
-}
-else if{
+else if
     (number % 3 === 0) {
         return "Fizz";
 }
-}
-else if{
+else if
     (number % 5 === 0) {
         return "Buzz";
-}
 }
 else{
     return(number)
--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -1,19 +1,26 @@
-describe("My Fizzbuzz", function() {
+describe("My fizzBuzz", function() {
     beforeEach(function(){ 
-        number = new fizzbuzz;
+        fizzbuzz = new fizzBuzz;
     });
-    
-
-    describe("test is number is divisible by 3 and 5, then return 'FizzBuzz'", function() {
-        it("should return 'FizzBuzz'", function() {
-            var expect=15;
-            expect(calc.value).toBe();
+        describe("Returns number, fizz, buzz or fizzbuzz", function() {
+        it("should exist", function() {
+            expect(fizzBuzz).toBeDefined();
         });
-        it("should return 'Fizz'", function() {
-            var expect=15;
-            expect(calc.value).toBe();
+        it("should return 'Fizz' when called as fizzBuzz(9)", function() {
+            var result=fizzBuzz(9);
+            expect(result).toBe('Fizz');
         });
-        it("should return 'Buzz'", function() {
-            var expect=15;
-            expect(calc.value).toBe();
+        it("should return 'Buzz'when called as fizzBuzz(10)", function() {
+            var result=fizzBuzz(10);
+            expect(result).toBe('Buzz');
         });
+           it("should return FizzBuzz when called as fizzBuzz(15)", function() {
+            var result = fizzBuzz(15)
+            expect(result).toBe('FizzBuzz');
+        });
+        it("should return 2 when called as fizzBuzz(2)", function() {
+            var result = fizzBuzz(2)
+            expect(result).toBe(2);
+        });
+            });
+});
